14c84ca120be5915ee8206d52d7a4c4e0f20de7b
Willian <willian.alves.b15@gmail.com>
2022-08-29 16:20:23 -0300

    feat: created repository

--- backend/src/interfaces/ITask.ts
+++ backend/src/interfaces/ITask.ts
@@ -3,3 +3,10 @@ export interface ITask {
   task: string;
   status: string;
 }
+
+export interface ITaskModel {
+  findAll(): Promise<ITask[] | null>;
+  create(body: ITask): Promise<ITask | null>;
+  update(id: string, body: ITask): Promise<unknown>;
+  delete(id: string): Promise<unknown>;
+}
---
+++ backend/src/repository/tasks.repository.ts
@@ -0,0 +1,28 @@
+import Model from '../database/models/tasks';
+import { ITask, ITaskModel } from '../interfaces/ITask';
+
+export default class TasksRepository implements ITaskModel {
+  constructor(private model = Model) {
+    this.model = model;
+  }
+
+  async findAll(): Promise<ITask[] | null> {
+    const tasks = await this.model.findAll();
+    return tasks;
+  }
+
+  async create(body: ITask): Promise<ITask | null> {
+    const task = await this.model.create({ task: body.task, status: body.status });
+    return task;
+  }
+  // volta aqui para analisar o retorno dessa função!
+  async update(id: string, body: ITask): Promise<unknown> {
+    const task = await this.model.update({ task: body.task, status: body.status }, { where: { id } });
+    return task;
+  }
+
+  async delete(id: string): Promise<unknown> {
+    const task = await this.model.destroy({ where: { id } });
+    return task;
+  }
+};
