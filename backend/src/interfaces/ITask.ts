f21d9c4e35b46f54706fb5761a091a5f083ef249
Willian <willian.alves.b15@gmail.com>
2022-08-29 16:43:39 -0300

    fix: in service created class TasksService

--- backend/src/interfaces/ITask.ts
+++ backend/src/interfaces/ITask.ts
@@ -10,3 +10,10 @@ export interface ITaskModel {
   update(id: string, body: ITask): Promise<unknown>;
   delete(id: string): Promise<unknown>;
 }
+
+export interface ITaskService {
+  findAll(): Promise<ITask[] | null>;
+  create(body: ITask): Promise<ITask | null>;
+  update(id: string, body: ITask): Promise<unknown>;
+  delete(id: string): Promise<unknown>;
+}
